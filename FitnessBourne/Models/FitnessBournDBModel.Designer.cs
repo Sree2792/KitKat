11311630fb71875b961cd3ccd69db6a008ebc080
sjay0010 <sjay0010@DESKTOP-P8KCJRL>
2018-08-29 18:37:46 +1000

    Table created based on design

--- FitnessBourne/Models/FitnessBournDBModel.Designer.cs
+++ FitnessBourne/Models/FitnessBournDBModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\sjay0010\source\repos\FitnessBourne\FitnessBourne\Models\FitnessBournDBModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\sjay0010\source\repos\FitnessBourne\FitnessBourne\Models\FitnessBournDBModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
